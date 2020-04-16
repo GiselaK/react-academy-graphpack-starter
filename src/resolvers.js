e099c5eb40125b3763b9e74ad53ff731e77a09ed
Gisela Kottmeier <giselakottmeier@Giselas-Mac.local>
2020-04-16 12:48:17 -0700

    part one

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,7 +1,29 @@
+import data from "./data";
+
+const filterArticles = (searchText, articles) => {
+  if (searchText) {
+    return articles.filter((article => {
+      const articleBody = article.body.toLowerCase();
+      const search = searchText.toLowerCase();
+      const articleTitle = article.title.toLowerCase();
+
+      const titleIncludesText = title.indexOf(search) !== -1;
+      const bodyIncludesText = articleBody.indexOf(search) !== -1;
+      return titleIncludesText || bodyIncludesText;
+    }));
+  }
+  return articles;
+};
+
 const resolvers = {
   Query: {
-    hello: () => `Hello World!`
+    articles: (parent, { searchText }) => {filterArticles(searchText, data.articles)},
+    users: (parent, { limit }) => limit ? data.users.slice(0, limit) : data.users,
+    user: (parent, { id }) => data.users.find(user => user.id === id),
+    article: (parent, { id }) =>
+      data.articles.find(article => article.id === id)
   }
 };
 
 export default resolvers;
+      
